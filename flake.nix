2bbdde96c94e9cf44321c75e87caf6c97d594467
Jason Felice <jason.m.felice@gmail.com>
2022-05-25 09:15:59 -0400

    Simplify flake

--- flake.nix
+++ flake.nix
@@ -1,25 +1,21 @@
 {
   description = "Keyboard access to focus and arrange mostly-tiled windows";
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs = {};
   outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachSystem ["x86_64-darwin"] (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        window-sigils = pkgs.callPackage ./derivation.nix {};
-      in {
-        packages = {
-          default = window-sigils;
-          inherit window-sigils;
-        };
-        checks = {
-          test = pkgs.runCommandNoCC "window-sigils-test" {} ''
-            mkdir -p $out
-            : ${window-sigils}
-          '';
-        };
-    })) // {
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-darwin;
+      window-sigils = pkgs.callPackage ./derivation.nix {};
+    in {
+      packages.x86_64-darwin = {
+        default = window-sigils;
+        inherit window-sigils;
+      };
+      checks.x86_64-darwin = {
+        test = pkgs.runCommandNoCC "window-sigils-test" {} ''
+          mkdir -p $out
+          : ${window-sigils}
+        '';
+      };
       overlays.default = final: prev: {
         window-sigils = prev.callPackage ./derivation.nix {};
       };
